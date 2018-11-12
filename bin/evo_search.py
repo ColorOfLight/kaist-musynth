6b759989143da1cb3bc00ff9f29bcd10bd38e7d4
obiwan96 <obiwan96@kaist.ac.kr>
2018-11-12 22:40:01 +0900

    fitnessfunction withou sanity check. modificate for readabillity

--- bin/evo_search.py
+++ bin/evo_search.py
@@ -10,36 +10,39 @@ if not succeed:
   return None
 return synth_code
 
-def seeding(candidate,popul_size):
+def seeding(candidate, popul_size):
   return seed_pool
 #make seed 
 
-def fitness(draft_code,runtime_limit,input_data,output_data):
+def fitness(draft_code, runtime_limit, input_data, output_data):
 #draft_code should be code whose hole is fulled with candidate. Not AST!!
   
-  score=0
+  score = 0.0
   #score will be 0~3. If score is 3, it will ends. perfect!
 
-  #sanity_check
+  #sanity_check just run. If output successfully come out within 1second, pass sanity check.
+  #we should find the way to limit running time. maybe using process?
+  #do in hackaton.
   if not_sanity_check_succeed:
-    return 0
+    return 0.0
   else:
-    score+=1.0
+    score += 1.0
 
-  f=open('test.py','w')
-  f.write(draft_code)
-  f.close()
+  with f=open('test.py','w'):
+    f.write(draft_code)
+    f.close()
 
-  test_num=len(input_data)
-  test_score=0
+  test_num = len(input_data)
+  test_score = 0.0
   for i in range(test_num):
-    start_time=time.time()
-    result = subprocess.check_output ('python test.py '+input_data[i] , shell=True)
-    if time.time()-start_time<runtime_limit:
-      test_score+=1
-    if result[:-2]==output_data[i]: #result may have '\n' in end. so remove it. 
-      test_score+=1
+    start_time = time.time()
+    result = subprocess.check_output ('python test.py '+input_data[i] , shell=True).strip()
+     #result may have '\n' in end. so remove it. 
+    if time.time() - start_time < runtime_limit:
+      test_score += 1.0
+    if result == output_data[i]:
+      test_score += 1.0
   
-  score+=float(test_score/test_num)
+  score += test_score / test_num
   return score
     

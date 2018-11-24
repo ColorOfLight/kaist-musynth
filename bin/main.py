d441afb37ad43434fab4c84883e661ffb64ba721
Seongho Park <bluetosh7@gmail.com>
2018-11-24 16:13:57 +0900

    fix: fix bugs in ast_manager

--- bin/main.py
+++ bin/main.py
@@ -1,34 +1,30 @@
 # from evo_search import run_evo
 # from read import get_draft_code, get_candidates
-from ast_manager import fill_hole
+from ast_manager import fill_hole, generate_candidates
+from read import read_dataset
+from evo_search import run_evo
 import astor
 import ast
+import os
 
 # >> python bin/main.py draft_fn test_direc
-draft_fn = '../sample.py'
-output_fn = '../output.py'
+test_name = 'insertion-sort'
+
 
 # Get Input code 
-# draft_code = get_draft_code(draft_fn)
-hole_tree = astor.code_to_ast.parse_file(draft_fn)
+hole_tree = astor.code_to_ast.parse_file(os.path.join('../testcase', test_name + '.py'))
 
-input_node = ast.Import(names=[ast.alias(name='quux', asname=None)])
-hole_tree = fill_hole(input_node, hole_tree)
-
-with open(output_fn, 'w') as writefile:
-  writefile.write(astor.to_source(hole_tree))
-
-'''
 # Get dataset
-input_data, output_data = read_dataset(test_direc)
+input_data, output_data = read_dataset(os.path.join('../code-pool', test_name))
 
 # Read hole and get Candidates
 # hole_comment: sort /// A
-candidates = get_candidates(hole_comment)
+cand_dict, func_dict = generate_candidates(os.path.join('../code-pool', test_name))
+
 
 # If it fails, return null
-synth_code = run_evo(draft_code, input_data, output_data, candidates)
-
+synth_code = run_evo(hole_tree, input_data, output_data, input_data, output_data)
+'''
 if synth_code:
   # write_new_file
   print("Success")

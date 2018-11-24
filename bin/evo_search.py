178361d85ddedba1b024382a430d5a9817e510b7
obiwan96 <obiwan96@kaist.ac.kr>
2018-11-24 21:33:28 +0900

    lexicase coding end

--- bin/evo_search.py
+++ bin/evo_search.py
@@ -1,4 +1,5 @@
 import subprocess
+import ast_manager
 import sys
 
 class codeCand(Object):
@@ -24,20 +25,19 @@ def run_evo(
 #input_data and output_data are string. How about candidates and draft_code?
 
   seed_pool,used_cand_list = seeding(cand_list, func_list, popul_size)
-  max_score=0.0
   for i in range(max_iteration):
     #mutate
-    #seed_pool = mutation(seed_pool,mut_porb) - Tempo code.
+    #seed_pool += mutation(seed_pool,mut_porb) - Tempo code.
     #seed_pool will be 150Gae
-    max_score, seed_pool = lexicase_test([input_data, output_data], seed_pool, runtime_limit)
+    seed_pool = lexicase_test([input_data, output_data], hole_tree, seed_pool, func_dict, runtime_limit)
 
     #left popul_size candidates. 0score candidates should be sorted randomly.
     seed_pool = seed_pool[:popul_size]
-    if max_score==1.0:
+    if seed_pool[0].get_score()==1.0:
       #return should be fixed later.
       return seed_pool[0]
     if i%10==0:
-      print('%dth iteration. max_score is %d'%(i,max_score))
+      print('%dth iteration. max_score is %d'%(i,seed_pool[0].get_score()))
 
   '''if not succeed:
     return None
@@ -50,14 +50,65 @@ def seeding(candidate, func_list, popul_size):
   #len(used_cand_list) should be same with popul_size
   return seed_pool, used_cand_list
 
-def lexicase_test(test_case, hole_tree, seed_list, runtime_limit)
+def lexicase_test(test_case, hole_tree, seed_pool, func_dict, runtime_limit):
   #by Suk
-  test_num = len(test_case)
+  pool_size=len(seed_pool)
+  scoring_start_index=0
+  input_data, output_data = test_case
+  test_num = len(input_data)
+
+  for i in range(pool_size):
+    if seed_pool[i].get_score()==None:#Assume that all none scoring seeds locate back.
+      scoring_start_index=i
+      break
+  for i in range(scoring_start_index,pool_size):
+    seed_pool[i].set_score(0.0)
+  #If scoring_start_index is 0, all seeds should be scroing.
+
   for i in range(test_num):
-    break
+    scoring_end_index_list=[]
+    for j in range(scoring_start_index,pool_size):
+      filled_code=ast_manager.fill_hole(hole_tree, seed_pool[j], func_dict)#will be changed after Sungho complete fill_hole function.
+      with open('temp_test.py','w') as f:
+        f.write(filled_code)
+        f.close()
+      try:
+        #using python3 but with virtual env.
+        result = subprocess.check_output ('python temp_test.py', input=input_data[i], 
+        shell=True, timeout=runtime_limit, stderr=sys.STDERR, universal_newlines=True).strip()
+        #Why strip?: result may have '\n' in end. so remove it. 
+        if result == output_data[i]:
+          seed_pool[j].set_score(seed_pool[j].get_score()+1.0)
+        else:
+          seed_pool[j].set_score(seed_pool[j].get_score()+0.5)
+        if seed_pool[j].get_score==test_num: #All test case pass.
+          seed_pool[j].set_score(1.0)
+          seed_pool[0]=seed_pool[j]
+          return seed_pool
+      except Exception:
+        scoring_end_index_list.append(j)
+        #Time out, Runtime error, etc. Sanity check fail. scoring end.
 
-  return max_score, seed_list
+    for j in scoring_end_index_list:
+      if i > 1:
+        seed_pool[j].set_score(seed_pool[j].get_score()/i)
+      seed_pool[j], seed_pool[scoring_start_index] = seed_pool[scoring_start_index], seed_pool[j]
+      scoring_start_index+=1
 
+  for i in range(scoring_start_index, pool_size):
+    seed_pool[i].set_score(seed_pool[i].get_score()/test_num)
+  
+  #Scoring end. Sorting.
+  i=1
+  while i < pool_size:
+    tmp=seed_pool[i]
+    j=i-1
+    while seed_pool[j].get_score() < tmp.get_score() and j>-1:
+      seed_pool[j+1]=seed_pool[j]
+      j= j-1
+    seed_pool[j+1]=tmp
+    i+=1
+  return seed_pool
 
 def fitness(draft_code, runtime_limit, input_data, output_data):
 #input_data and output_data is string. just read from file.

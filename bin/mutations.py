e5aa578e6070ab45d04d0ea86815fb66ceb866a4
Seongho Park <bluetosh7@gmail.com>
2018-11-25 03:32:41 +0900

    feat: add Num mutations

--- bin/mutations.py
+++ bin/mutations.py
@@ -16,14 +16,39 @@ class changeName(ast.NodeTransformer):
       self.name_num += 1
     return node
 
+class makeNumOff(ast.NodeTransformer):
+  def __init__(self, change_num):
+    ast.NodeTransformer.__init__(self)
+    self.change_num = change_num
+    self.name_num = 0
+
+  def visit_Num(self, node):
+    if (self.change_num == self.name_num):
+      node.n = node.n - 1
+      self.name_num += 1
+    elif (self.change_num > self.name_num):
+      self.name_num += 1
+    return node
+
+class changeNum(ast.NodeTransformer):
+  def __init__(self, max_const, change_num):
+    ast.NodeTransformer.__init__(self)
+    self.max_const = max_const
+    self.change_num = change_num
+    self.name_num = 0
+
+  def visit_Num(self, node):
+    if (self.change_num == self.name_num):
+      node.n = self.max_const
+      self.name_num += 1
+    elif (self.change_num > self.name_num):
+      self.name_num += 1
+    return node
+
 def rebind_variable(cand, variable_list):
   cand_node = cand.get_node()
   
-  name_num = 0
-  for node in ast.walk(cand_node):
-    if isinstance(node, ast.Name):
-      name_num += 1
-
+  name_num = _count_type_nodes(cand_node, ast.Name)
   if name_num <= 0:
     return None
 
@@ -32,13 +57,34 @@ def rebind_variable(cand, variable_list):
   return changeName(input_name, change_num).visit(cand_node)
 
 def fix_off_by_one(cand):
-  return
+  cand_node = cand.get_node()
+
+  node_num = _count_type_nodes(cand_node, ast.Num)
+  if node_num <= 0:
+    return None
+
+  change_num = random.randrange(0, node_num)
+  return makeNumOff(change_num).visit(cand_node)
 
 def replace_variable_with_constant(cand, max_const):
-  return
+  cand_node = cand.get_node()
+
+  node_num = _count_type_nodes(cand_node, ast.Num)
+  if node_num <= 0:
+    return None
+
+  change_num = random.randrange(0, node_num)
+  return changeNum(max_const, change_num).visit(cand_node)
 
 def delete_statement(cand):
   return
 
 def insert_new_statement(cand, cand_list):
   return
+
+def _count_type_nodes(tree, ast_type):
+  node_num = 0
+  for node in ast.walk(tree):
+    if isinstance(node, ast_type):
+      node_num += 1
+  return node_num
